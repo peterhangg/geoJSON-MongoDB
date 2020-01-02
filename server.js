c3c36858225df775156e82c553a012f119c9e457
Peter Hang <peterhanggg@gmail.com>
2020-01-02 17:41:32 -0500

    setup a stores route

--- server.js
+++ server.js
@@ -3,14 +3,18 @@ const express = require("express");
 const dotenv = require("dotenv");
 const cors = require("cors");
 const PORT = process.env.PORT || 8080;
-
 const app = express();
+
+// load env vars
+dotenv.config({ path: "./config/config.env" });
 
 app.use(express.json());
 app.use(cors());
 
-// load env vars
-dotenv.config({ path: "./config/config.env" });
+// Routes
+app.use("/api/v1/stores", require("./routes/stores"));
+
+
 
 // Server
 app.listen(PORT, () => {
